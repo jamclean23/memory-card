8c0de839a8034f01ec4d36089268e8fae5f416c8
jamclean23 <jamclean23@gmail.com>
2023-03-29 15:33:11 -0400

    Update pages-push.bat to include default parameters

--- pages-push.bat
+++ pages-push.bat
@@ -1,3 +1,9 @@
+IF "%1"=="" (
+    set comment="GH-Pages Push"
+) ELSE (
+    set comment=%1
+)
+
 git add dist -f
-git commit -m %1
+git commit -m %comment%
 git subtree push --prefix dist origin gh-pages
