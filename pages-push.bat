6e3230f5a021d3cacad12c4c8ec1aedec73033c6
jamclean23 <jamclean23@gmail.com>
2023-04-08 12:35:38 -0400

    Tweak batch file for pushing to gh-pages

--- pages-push.bat
+++ pages-push.bat
@@ -1,10 +1,18 @@
-IF %1 =="" (
-    set comment="GH-Pages Push"
-) 
- (
-    set comment=%1
+@echo off
+IF "%1"=="" (
+    echo No parameters have been provided.
+    set "default=GH-Pages push"
+    echo Setting comment to %default%
+    git add dist -f
+    git commit -m "%default%"
+    echo Pushing to Github Pages ...
+    git subtree push --prefix dist origin gh-pages
+    echo Done.
+) ELSE (
+    echo Setting comment to %1
+    git add dist -f
+    git commit -m "%1"
+    echo Pushing to Github Pages ...
+    git subtree push --prefix dist origin gh-pages
+    echo Done.
 )
-
-git add dist -f
-git commit -m %comment%
-git subtree push --prefix dist origin gh-pages
