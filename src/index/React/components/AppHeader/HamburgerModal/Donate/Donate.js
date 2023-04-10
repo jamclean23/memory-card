ba1752405c2a5941000ca29b47ba81f9a2957089
jamclean23 <jamclean23@gmail.com>
2023-04-10 12:13:13 -0400

    Add donate page

--- src/index/React/components/AppHeader/HamburgerModal/Donate/Donate.js
+++ src/index/React/components/AppHeader/HamburgerModal/Donate/Donate.js
@@ -2,10 +2,18 @@ import './donate.css';
 import React from 'react';
 
 function Donate (props) {
+
+    function handleDonateClick () {
+        window.open('https://www.buymeacoffee.com/rooseveltcat');
+    }
+
     return (
         <div className={'Donate'}>
             <article>
                 <h3>Donate</h3>
+                <h4>Like this app?</h4>
+                <p>We would love to keep producing quality outdoors content!<br/>Follow the link to support this app as well as more to come.</p>
+                <button className='donateLink' onClick={handleDonateClick}>Support us 😊</button>
             </article>
             <button className='exitDonate' onClick={props.handleSubMenuBack}>Back</button>
         </div>
