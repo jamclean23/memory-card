dee8b9ee8becdeabfcdcfad815fd088f06675a7f
jamclean23 <jamclean23@gmail.com>
2023-04-13 10:50:16 -0400

    Add platform detection

--- src/index/index.js
+++ src/index/index.js
@@ -13,6 +13,14 @@ import { createRoot } from 'react-dom/client';
 // Main React App
 import { App } from './React/App.js';
 
+// ====== DETECT BROWSER ======
+
+console.log(navigator.platform);
+if (/iPhone/.test(navigator.platform) || /iPad/.test(navigator.platform)) {
+    window.location.replace("https://jamclean23.github.io/id-that-tree-safari/");
+}
+
+
 // ====== WINDOW ======
 
 window.oncontextmenu = function(event) {
